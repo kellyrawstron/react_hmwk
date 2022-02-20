f3c6fb70261b7d2c8339e68a8f73c60238188f32
Kelly Rawstron <code.kelly8@gmail.com>
2022-02-20 12:09:30 +0000

    react hmwk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    // 1️⃣
+import './App.css';           // 2️⃣
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {              // 3️⃣
+  // we can write JS here :)  // 4️⃣
+  const name = "kelly";
+  const age = 31;
+  return (     
 
-export default App;
+  
+                 // 5️⃣
+    <div>
+    <h1 className="title">Hello World!</h1>
+    <p> My name is {name} and i'm {age} years old</p>
+  </div>
+       // 6️⃣
+  );                          // 7️⃣
+}                             // 8️⃣
+
+export default App;           // 9️⃣
